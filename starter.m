400d613aa67f7359dccaa1fc27ebd23fee5f48ff
Medhini Narasimhan <medhini95@gmail.com>
2017-10-26 22:53:59 -0500

    Minor fix

--- starter.m
+++ starter.m
@@ -7,10 +7,10 @@ im4 = imread('proj4_materials/4.jpg');
 im5 = imread('proj4_materials/5.jpg');
 
 im1 = imresize(im1, [375,375],'bilinear');
-im2 = imresize(im1, [375,375],'bilinear');
-im3 = imresize(im1, [375,375],'bilinear');
-im4 = imresize(im1, [375,375],'bilinear');
-im5 = imresize(im1, [375,375],'bilinear');
+im2 = imresize(im2, [375,375],'bilinear');
+im3 = imresize(im3, [375,375],'bilinear');
+im4 = imresize(im4, [375,375],'bilinear');
+im5 = imresize(im5, [375,375],'bilinear');
 
 ldrs = cat(4,im1, im2, im3, im4, im5);
 x = ones(1,5);
