e50b91a8d56802469111fa1d2594603663c9e928
Medhini Narasimhan <medhini95@gmail.com>
2017-10-27 18:28:39 -0500

    Added tonemap and log radiance

--- makehdr_over_under.m
+++ makehdr_over_under.m
@@ -1,4 +1,4 @@
-function hdr_over_under = makehdr_over_under(ldrs, exposures)
+function makehdr_over_under(ldrs, exposures)
 %     ldrs is an m x n x 3 x k matrix which can be created with ldrs = cat(4, ldr1, ldr2, ...);
 %     exposures is a vector of exposure times (in seconds) corresponding to ldrs
     [exposures,sortexp] = sort(reshape(exposures,1,1,1,[]));
@@ -13,26 +13,22 @@ function hdr_over_under = makehdr_over_under(ldrs, exposures)
        im_r = w(ldrs(:,:,1,i));
        im_g = w(ldrs(:,:,2,i));
        im_b = w(ldrs(:,:,3,i));
-      
-       im_r = double(im_r)./exposures(i);
-       im_g = double(im_g)./exposures(i);
-       im_b = double(im_b)./exposures(i);
        
-       hdr_over_under(:,:,1) = hdr_over_under(:,:,1) + im_r;
-       hdr_over_under(:,:,2) = hdr_over_under(:,:,2) + im_g;
-       hdr_over_under(:,:,3) = hdr_over_under(:,:,3) + im_b;
+       hdr_over_under(:,:,1) = hdr_over_under(:,:,1) + im_r./exposures(i);
+       hdr_over_under(:,:,2) = hdr_over_under(:,:,2) + im_g./exposures(i);
+       hdr_over_under(:,:,3) = hdr_over_under(:,:,3) + im_b./exposures(i);
     end
     
     hdr_over_under(:,:,1) = hdr_over_under(:,:,1)./5;
     hdr_over_under(:,:,2) = hdr_over_under(:,:,2)./5;
     hdr_over_under(:,:,3) = hdr_over_under(:,:,3)./5;
-        
-    hdrwrite(hdr_over_under, 'hdr-2.hdr');
     
-    hdr_over_under(:,:,1) = (hdr_over_under(:,:,1) - min(min(hdr_over_under(:,:,1))))/(max(max(hdr_over_under(:,:,1))) - min(min(hdr_over_under(:,:,1))));
-    hdr_over_under(:,:,2) = (hdr_over_under(:,:,2) - min(min(hdr_over_under(:,:,2))))/(max(max(hdr_over_under(:,:,2))) - min(min(hdr_over_under(:,:,2))));
-    hdr_over_under(:,:,3) = (hdr_over_under(:,:,3) - min(min(hdr_over_under(:,:,3))))/(max(max(hdr_over_under(:,:,3))) - min(min(hdr_over_under(:,:,3))));
+    hdrwrite(hdr_over_under, 'hdr_over_under.hdr');
+    
+%     hdr_over_under(:,:,1) = (hdr_over_under(:,:,1) - min(min(hdr_over_under(:,:,1))))/(max(max(hdr_over_under(:,:,1))) - min(min(hdr_over_under(:,:,1))));
+%     hdr_over_under(:,:,2) = (hdr_over_under(:,:,2) - min(min(hdr_over_under(:,:,2))))/(max(max(hdr_over_under(:,:,2))) - min(min(hdr_over_under(:,:,2))));
+%     hdr_over_under(:,:,3) = (hdr_over_under(:,:,3) - min(min(hdr_over_under(:,:,3))))/(max(max(hdr_over_under(:,:,3))) - min(min(hdr_over_under(:,:,3))));
 
-    figure(2), imshow(hdr_over_under)
-    
+    figure(1), imshow(tonemap(hdr_over_under))
+    figure(2), imshow(tonemap(log(hdr_over_under)))
 end
